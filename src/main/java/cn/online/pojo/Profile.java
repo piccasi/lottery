128bea37923e4eb1bacb67b7aa84b5113127f292
piccasi <loveasmilai8@163.com>
2017-12-20 09:47:26 +0800

    个人信息

---
+++ src/main/java/cn/online/pojo/Profile.java
@@ -0,0 +1,21 @@
+package cn.online.pojo;
+/** 
+ * @author  作者 E-mail: 
+ * @date 创建时间：2017-12-8 上午11:22:12 
+ * @version 1.0 
+ * @parameter  
+ * @since  
+ * @return  
+ */
+public class Profile {
+	private String nikeName;
+
+	public String getNikeName() {
+		return nikeName;
+	}
+
+	public void setNikeName(String nikeName) {
+		this.nikeName = nikeName;
+	}
+
+}
